fb407cf84aea60cc6ec87df7fff1842f225e5312
Vedant-Pujari <vedantpujari15@gmail.com>
2026-02-21 22:15:33 +0530

    Added

---
+++ EvenOdd1.java
@@ -0,0 +1,16 @@
+class EvenOdd1
+{
+	public static void main(String args[])
+	{
+		int num=8;
+		System.out.println("Program to find number is even of odd");
+		if(num%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+}
