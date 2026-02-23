1dc463eaff8e12d800f8f752fde2c047526c28c2
Vedant-Pujari <vedantpujari15@gmail.com>
2026-02-23 23:04:56 +0530

    Added

---
+++ Mai.java
@@ -0,0 +1,12 @@
+/*<applet code = "Mai" width = 500 height = 500></applet>*/
+
+import java.awt.*;
+import java.applet.*;
+
+public class Mai extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("SREE KARI",100,200);
+	}
+}
