f1a2ac5d25725a0a72c0b10af93e594aea7ebed5
Vedant-Pujari <vedantpujari15@gmail.com>
2026-02-23 23:04:32 +0530

    Added

---
+++ Largest.java
@@ -0,0 +1,18 @@
+class Largest
+{
+	public static void main(String args[])
+	{
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
+
+		if(n1>n2)
+		{
+			System.out.println(n1 + " is large");
+
+		}
+		else
+		{
+			System.out.println(n2 + " is large");
+		}
+	}
+}
